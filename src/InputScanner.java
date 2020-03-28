aad0780190d91f6f9387aff4a17b9d161092b225
lukasz-borowiecki <lukaszgda@wp.pl>
2020-03-28 16:50:30 +0000

    Initial Commit

---
+++ src/InputScanner.java
@@ -0,0 +1,15 @@
+package inputscanner;
+import java.util.Scanner;
+
+public class InputScanner {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter your address: ");
+        Scanner scanner = new Scanner(System.in);
+        String address = scanner.nextLine();
+        System.out.println("You live at: " + address);
+    }
+
+}
